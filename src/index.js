d8a43d249b28faeb2a9788054b59c39a5ce49948
Raimedas Simutis <raimedas.simutis@gmail.com>
2019-10-21 18:59:09 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './assets/styles/style.scss'
+
+
+
+import Icon from './assets/images/image.jpg'
+
+const img = new Image()
+img.src = Icon;
+
+let body = document.querySelector('body');
+let div = document.createElement("div"); 
+div.appendChild(img);
+body.appendChild(div);
+
+
