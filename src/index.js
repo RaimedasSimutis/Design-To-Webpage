5e592287938c7c4555b64d7113c1441233d51057
Raimedas Simutis <raimedas.simutis@gmail.com>
2019-10-24 02:22:25 +0300

    added media breakpoints

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import './assets/styles/style.scss'
+//styles
+import './assets/styles/reset.scss';
+import './assets/styles/variables.scss';
+import './assets/styles/style.scss';
+import './assets/styles/media-breakpoints.scss';
+
+
+//images
+import './assets/images/logo.svg';
+import './assets/images/crayon.svg';
+import './assets/images/flag.png';
+import './assets/images/rocket.svg';
+import './assets/images/gears.png';
+import './assets/images/laptop1.png';
+import './assets/images/laptop2.png';
+import './assets/images/laptop3.png';
+import './assets/images/laptop4.png';
+import './assets/images/facebook.png';
+import './assets/images/twitter.png';
+import './assets/images/linkedin.png';
+import './assets/images/mail.png';
+import './assets/images/anne-hathaway.png';
+import './assets/images/kate-upton.png';
+import './assets/images/olivia-wilde.png';
+import './assets/images/ashley-greene.png';
+import './assets/images/chart1.png';
+import './assets/images/chart2.png';
+import './assets/images/chart3.png';
+import './assets/images/chart4.png';
 
 
 
-import Icon from './assets/images/image.jpg'
 
-const img = new Image()
-img.src = Icon;
 
-let body = document.querySelector('body');
-let div = document.createElement("div"); 
-div.appendChild(img);
-body.appendChild(div);
+
+
+
+
+
 
 
